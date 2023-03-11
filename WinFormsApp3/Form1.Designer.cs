8ed6fa7892693a68f40d815877a6168630aa2291
MelekPashazada <Melekpashazada@gmail.com>
2023-03-11 22:46:57 +0400

    11 mart 3

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 135);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tarix";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(88, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 104);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(303, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 135);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "+3";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(515, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 135);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Age";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(536, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(536, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(577, 345);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 58);
+            this.button4.TabIndex = 10;
+            this.button4.Text = ">";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private Button button2;
+        private Label label3;
+        private Label label4;
+        private Button button3;
+        private Label label5;
+        private Label label6;
+        private Button button4;
     }
 }
--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -6,5 +6,51 @@ namespace WinFormsApp3
         {
             InitializeComponent();
         }
+        DateTime date = DateTime.Now;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            label1.Text=date.ToString();
+            listBox1.Items.Add(date.ToLongDateString());
+            listBox1.Items.Add(date.ToShortDateString());
+            listBox1.Items.Add(date.Year);
+            listBox1.Items.Add(date.Month);
+            listBox1.Items.Add(date.Day);
+            listBox1.Items.Add(date.Hour);
+            listBox1.Items.Add(date.ToString(""));
+
+
+
+
+            DateTime date1 = new DateTime(2016, 05, 23);
+            label2.Text= date1.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text=date.AddDays(3).ToString();
+            label4.Text = date.AddYears(3).ToString();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DateTime tevellud = new DateTime(01,02,1991);
+            DateTime tarix_bugun = new DateTime(03,11,2023);
+
+            
+
+            TimeSpan age = tarix_bugun - tevellud;
+            label6.Text = (Convert.ToInt32(tarix_bugun.Year) - Convert.ToInt32(tevellud.Year)).ToString();
+          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 form2=new Form2();
+            form2.Show();
+            this.Hide();
+        }
     }
 }
